3c8a90d3ce8f68370a420800f909c7d112055631
Alexei G <alekseigolovanov89@gmail.com>
2023-02-23 12:19:04 +0200

    added iframe with some settings for safely handling untrusted code

--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,32 @@
 import * as esbuild from "esbuild-wasm";
 import { useState, useEffect, useRef } from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
 import { unpkgPathPlugin } from "./plugin/unpkg-path-plugin";
 import { fetchPlugin } from "./plugin/fetch-plugin";
 
-const HTMLElement = document.querySelector("#root") as HTMLDivElement;
-
-const root = createRoot(HTMLElement);
-
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
 
-  const startingService = async () => {
+  const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
+      wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
     });
   };
-
   useEffect(() => {
-    startingService();
+    startService();
   }, []);
 
-  const onCLick = async () => {
-    if (!ref.current) return;
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
 
-    //bundle process | build bundle
+    iframe.current.srcdoc = html;
+
     const result = await ref.current.build({
       entryPoints: ["index.js"],
       bundle: true,
@@ -38,8 +37,30 @@ const App = () => {
         global: "window",
       },
     });
-    setCode(result.outputFiles[0].text);
+
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
   };
+
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            try {
+              eval(event.data);
+            } catch (err) {
+              const root = document.querySelector('#root');
+              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+              console.error(err);
+            }
+          }, false);
+        </script>
+      </body>
+    </html>
+  `;
 
   return (
     <div>
@@ -48,11 +69,12 @@ const App = () => {
         onChange={(e) => setInput(e.target.value)}
       ></textarea>
       <div>
-        <button onClick={onCLick}>Submit</button>
+        <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
+      <iframe ref={iframe} sandbox="allow-scripts" srcDoc={html} />
     </div>
   );
 };
 
-root.render(<App />);
+ReactDOM.render(<App />, document.querySelector("#root"));
--- src/plugin/unpkg-path-plugin.ts
+++ src/plugin/unpkg-path-plugin.ts
@@ -1,31 +1,30 @@
-import * as esbuild from "esbuild-wasm";
+import * as esbuild from 'esbuild-wasm';
 
 export const unpkgPathPlugin = () => {
   return {
-    name: "unpkg-path-plugin", // for debug
+    name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
-      // Handle root file of 'index.js'
+      // Handle root entry file of 'index.js'
       build.onResolve({ filter: /(^index\.js$)/ }, () => {
-        return { path: "index.js", namespace: "a" };
+        return { path: 'index.js', namespace: 'a' };
       });
 
-      //Handle relative paths in module
+      // Handle relative paths in a module
       build.onResolve({ filter: /^\.+\// }, (args: any) => {
         return {
-          namespace: "a",
-          path: new URL(args.path, "https://unpkg.com" + args.resolveDir + "/")
+          namespace: 'a',
+          path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/')
             .href,
         };
       });
 
-      //Handle main file of a module
+      // Handle main file of a module
       build.onResolve({ filter: /.*/ }, async (args: any) => {
         return {
-          namespace: "a",
+          namespace: 'a',
           path: `https://unpkg.com/${args.path}`,
         };
       });
-
     },
   };
 };
