14e4f8bf6f5d99df0e62b42ecf66335799f3ce09
Alexei G <alekseigolovanov89@gmail.com>
2023-03-07 12:38:45 +0200

    added small changes for error handle message in iframe

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -5,12 +5,14 @@ import bundler from "../builder";
 import Resizable from "./resizable";
 const CodeCell = () => {
   const [input, setInput] = useState("");
+  const [err, setErr] = useState("")
   const [code, setCode] = useState("");
 
   useEffect(() => {
     const timer = setTimeout(async () => {
       const output = await bundler(input);
-      setCode(output);
+      setCode(output.code);
+      setErr(output.err)
     }, 1000);
 
     return () => {
@@ -27,7 +29,7 @@ const CodeCell = () => {
             onChange={(value) => setInput(value)}
           />
         </Resizable>
-        <Preview code={code} />
+        <Preview code={code} err={err} />
       </div>
     </Resizable>
   );
